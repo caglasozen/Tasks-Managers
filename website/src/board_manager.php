9a299ff5f9dc620ee50abdcf7dec161d7f1e4daa
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2019-05-19 21:12:52 +0300

    Updated board

--- website/src/board_manager.php
+++ website/src/board_manager.php
@@ -13,32 +13,65 @@
 		header('Location: index.php');
 	}
 	
+	function insertList( $newListName, $newListDesc, $newListDue, $board_id )
+	{
+		$cred_query = "insert into List(name, description, issue_date,due_date,board_id) values()";
+		
+		$result = mysqli_query($mysqli,$cred_query);
+		$flag = mysqli_fetch_row($result);
+		
+		
+		if($flag === null  ){
+			echo '<script language="javascript">';
+			echo 'alert("No such user with the given password in the system")';
+			echo '</script>';
+		}else{
+			
+			$_SESSION['user_name'] = $user;
+			$_SESSION['password'] = $pass;
+			header('Location: index.php');
+		}
+	}
+	
+	/*
 	$user_id = $_SESSION['user_id'];
 	$proj_id = $_SESSION['project_id'];
 	$board_id = $_SESSION['board_id'];
+	*/
+	
+	$board_id = 1;
 	
 	/*
 	//Fetching project information.
 	$query_proj_info = "SELECT * FROM Project WHERE id = 1";
 	$result_pr = mysqli_query($mysqli, $query_proj_info);
 	$row_pr = mysqli_fetch_assoc($result_pr);
+	*/
 	
 	//Fetching board information.
-	$query_board_info = "SELECT * FROM Board WHERE id = $board_id";
+	$query_board_info = "SELECT * FROM Board WHERE id = '" . $board_id . "' ";
 	$result_bo = mysqli_query($mysqli, $query_board_info);
-	$row_bo = mysqli_fetch_assoc($result_bo);
+	$row_bo = mysqli_fetch_array($result_bo);
 	
 	//Fetching list information.
-	$query_list_info = "SELECT * FROM List WHERE board_ID = $board_id";
+	$query_list_info = "SELECT * FROM List WHERE board_ID = '" . $board_id . "'";
 	$result_li = mysqli_query($mysqli, $query_list_info);
-	$row_li = mysqli_fetch_assoc($result_li);
-	 
 	
+	/*
 	if(!isset($user_id)){
 		header('Location: form.php');
-	}*/
+	}
+	 */
 	
+	if(isset($_POST['submitList'])){
+		
+		$newListName = mysqli_real_escape_string($mysqli,$_POST['listName']);
+		$newListDesc = mysqli_real_escape_string($mysqli,$_POST['listDesc']);
+		$newListDue = mysqli_real_escape_string($mysqli,$_POST['listDue']);
+		
+		insertList(	$newListName, $newListDesc, $newListDue, $board_id);
 
+	}
 	
 	if(array_key_exists('Logout',$_POST)){
 		logOut();
@@ -75,7 +108,6 @@ function closeNewList() {
 	document.getElementById("ListForm").style.display = "none";
 }
 
-
 </script>
 
 
@@ -85,7 +117,7 @@ function closeNewList() {
 
 <div class="boardBox">
 
-<h3> Board 1 </h3>
+<h3> Board <?php echo $board_id ?> </h3>
 
 <div class="cornBut">
 
@@ -93,70 +125,45 @@ function closeNewList() {
 
 </div>
 
-	<div class="column">
-<div class="cornBut">
+<?php
+while ($row = mysqli_fetch_array($result_li))
+{
+	$l_name = $row['name'];
+	$l_desc = $row['description'];
+	$l_due = $row['due_date'];
+	
+	echo '<div class="column">';
+	echo '<div class="cornBut">';
+	
+	echo '<button class="newcardBut" onclick="newCard()">+</button>';
+	
+	
+	echo '</div>';
+	
 
-<button class="newcardBut" onclick="newCard()">+</button>
+	echo '<h3>List '. $l_name .'  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>';
+	echo '<div style="overflow: scroll;">';
 
-</div>
+	echo '<p>'. $l_desc .'</p>';
+	echo '</div>';
+	echo '<p> <b> Due Date </b>'. $l_due .'</p>';
 
-	<h3>List 1  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-		<div class="card">
-		<h3>Card 1  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-		<p>Some text</p>
-		<p>Some text</p>
-		</div>
 
-		<div class="card">
-		<h3>Card 2  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-		<p>Some text</p>
-		<p>Some text</p>
-		</div>
-
-	</div>
-
-	<div class="column">
-		<div class="cornBut">
-
-		<button class="newcardBut" onclick="newCard()">+</button>
-
-		</div>
-		<h3>List 2  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-		<div class="card">
-		<h3>Card 1  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-		<p>Some text</p>
-		<p>Some text</p>
-		</div>
-
-		<div class="card">
-		<h3>Card 2  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-		<p>Some text</p>
-		<p>Some text</p>
-		</div>
-
-	</div>
-
-	<div class="column">
-<div class="cornBut">
-
-<button class="newcardBut" onclick="newCard()">+</button>
-
-</div>
-	<h3>List 3  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-	<div class="card">
-	<h3>Card 1  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-	<p>Some text</p>
-	<p>Some text</p>
-	</div>
-
-	<div class="card">
-	<h3>Card 2  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>
-	<p>Some text</p>
-	<p>Some text</p>
-	</div>
-	</div>
-
-</div>
+	echo '<div class="card">';
+	echo '<h3>Card 1  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>';
+	echo '<p>Some text</p>';
+	echo '<p>Some text</p>';
+	echo '</div>';
+	
+	echo '<div class="card">';
+	echo '<h3>Card 2  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>';
+	echo '<p>Some text</p>';
+	echo '<p>Some text</p>';
+	echo '</div>';
+	
+	echo '</div>';
+}
+?>
 
 <div class="form-popup" id="cardForm">
 <form action="/board_manager.php" class="form-container">
@@ -174,7 +181,7 @@ function closeNewList() {
 </div>
 
 <div class="form-popup" id="ListForm">
-<form action="/board_manager.php" class="form-container" >
+<form  method="post" action="/board_manager.php" class="form-container" >
 <h1>Create New List</h1>
 
 <label for="listName"><b>List Name</b></label>
