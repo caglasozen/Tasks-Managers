cfcab1f495ed4d4499de1023a53dcb475bceddf8
Cagla Sozen <cagla.sozen@ug.bilkent.edu.tr>
2019-05-21 01:23:23 +0300

    delete list

--- website/src/board_leader.php
+++ website/src/board_leader.php
@@ -70,8 +70,27 @@
 		
 	}
 	
+	function deleteList()
+	{
+		
+		global $mysqli;
+		$oldListId = $_POST['delListBut'];
+		
+		$cred_query = "delete from list where id = '$oldListId'";
+		
+		
+		$flag = mysqli_query($mysqli, $cred_query);
+		
+		
+		if($flag === null ){
+			echo '<script language="javascript">';
+			echo 'alert("Not able to delete List")';
+			echo '</script>';
+		}else{
+			header("Refresh:0");
+		}
+	}
 	
-
 	if (array_key_exists('user_id',$_POST)){
 		$user_id = $_POST['user_id'];
 		$proj_id = $_POST['project_id'];
@@ -129,7 +148,9 @@
 		insertCard();
 	}
 
-
+	if(array_key_exists('delListBut',$_POST)){
+		deleteList();
+	}
 	
 	if(array_key_exists('Logout',$_POST)){
 		logOut();
@@ -202,6 +223,11 @@ while ($row = mysqli_fetch_array($result_li))
 	
 
 	echo '<h3>List '. $l_name .'  <a class="btn btn-sm btn-danger" href="#">X</a> </h3>';
+	
+	echo '<form method="post" action="board_leader.php" class="form-container">';
+	echo '<button class="newcardBut" name = delListBut type="submit" value ='. $l_id .'>X</button>';
+	echo '</form>';
+	
 	echo '<div style="overflow: scroll;">';
 
 	echo '<p>'. $l_desc .'</p>';
